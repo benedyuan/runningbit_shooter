e085b4ca9a564bc239ee7d57a04649f13c9dcfe8
benedyuan <89300553+benedyuan@users.noreply.github.com>
2021-08-21 19:25:29 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,8 @@
 input.onButtonPressed(Button.B, function () {
+    music.playMelody("A - - - A - - A ", 500)
+    music.playMelody("A - - - F - - C5 ", 500)
+    music.playMelody("A - - - F - - C5 ", 500)
+    music.playMelody("A A A - - - - - ", 500)
     SuperBit.Servo2(SuperBit.enServo.S1, 135)
     basic.pause(500)
     SuperBit.Servo2(SuperBit.enServo.S1, 105)
