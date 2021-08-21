50b09c0741612ed999fb1e407318bc3f92719596
benedyuan <89300553+benedyuan@users.noreply.github.com>
2021-08-21 19:20:50 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,6 @@
-
+input.onButtonPressed(Button.B, function () {
+    SuperBit.Servo2(SuperBit.enServo.S1, 135)
+    basic.pause(500)
+    SuperBit.Servo2(SuperBit.enServo.S1, 105)
+})
+SuperBit.Servo2(SuperBit.enServo.S1, 105)
